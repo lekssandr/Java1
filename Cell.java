c94f308e18a723f7f32e55f3ede25f93d5f0ac75
lekssandr <lekssandr@yandex.ru>
2022-02-11 14:34:04 +0300

    create project

---
+++ Cell.java
@@ -0,0 +1,40 @@
+import java.awt.Graphics2D;
+import java.awt.Color;
+
+/**
+ * Java. Classic Game Snake
+ *
+ * @author Aleksandr
+ * @version 08.08.2022
+ */
+ 
+public class Cell {
+    private int x, y;
+    private int size;
+    private Color color;
+
+    public Cell(int x, int y, int size, Color color) {
+        set(x, y);
+        this.size = size;
+        this.color = color;
+    }
+
+    public void set(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void paint(Graphics2D g) {
+        g.setColor(color);
+        g.fillOval(x * size, y * size,
+                size, size);
+    }
+}
---
+++ Food.java
@@ -0,0 +1,42 @@
+import java.awt.Color;
+import java.util.Random;
+
+/**
+ * Java. Classic Game Snake
+ *
+ *
+ * @author Aleksandr
+ * @version 08.08.2022
+ */
+
+public class Food extends Cell {
+    private Random random;
+    private Snake snake;
+
+    public Food(Snake snake) {
+        super(-1, -1, GameSnake.CELL_SIZE, GameSnake.FOOD_COLOR);
+        random = new Random();
+        this.snake = snake;
+    }
+
+    public boolean isFood(int x, int y) {
+        return (getX() == x) && (getY() == y);
+    }
+
+    public boolean isEaten() {
+        return getX() == -1;
+    }
+
+    public void eat() {
+        set(-1, -1);
+    }
+
+    public void appear() {
+        int x, y;
+        do {
+            x = random.nextInt(GameSnake.CANVAS_WIDTH);
+            y = random.nextInt(GameSnake.CANVAS_HEIGHT);
+        } while (snake.isInSnake(x, y));
+        set(x, y);
+    }
+}
